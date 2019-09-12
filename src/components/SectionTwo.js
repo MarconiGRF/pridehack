a1d6b94d93c0f341b4f245e091866b58f2a9294a
Marconi Gomes <mgrf@cin.ufpe.br>
2019-09-11 23:03:25 -0300

    Adding responsive layout.

---
+++ src/components/SectionTwo.js
@@ -0,0 +1,32 @@
+import React from "react"
+//import about from "../images/about.svg"
+
+const SectionTwo = () => (
+  <header>
+    <div>
+        <div className="maxWidthHalf floatItLeft">
+            <p className="textParagraphs">Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum, Lorem ipsum.</p>
+        </div>
+        <div className="maxWidthHalf floatItLeft">
+          <h1 className="textTitlesLight littleSpaceBot">PREMIAÇÃO</h1>
+          
+          <div className="floatItLeft spaceBot">
+            <p className="textTitles floatItLeft">1</p>
+            <p className="textSubTitles floatItLeft spaceLeft">Em breve</p>
+          </div>
+          
+          <div className="floatItLeft spaceBot">
+            <p className="textTitles floatItLeft">2</p>
+            <p className="textSubTitles floatItLeft spaceLeft">Em breve</p>
+          </div>
+
+          <div className="floatItLeft spaceBot">
+            <p className="textTitles floatItLeft">3</p>
+            <p className="textSubTitles floatItLeft spaceLeft">Em breve</p>
+          </div>
+        </div>
+    </div>
+  </header>
+)
+
+export default SectionTwo
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import ColorComponent from "../components/colorComponent"
 import Header from "./header"
+import SectionTwo from "./SectionTwo"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -19,7 +20,9 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <ColorComponent color="red"/>
+      <ColorComponent />
+      <SectionTwo/>
+      <ColorComponent />
     </>
   )
 }
